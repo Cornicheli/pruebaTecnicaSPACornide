d92073e01ff6a7e3eb116eb7996b75eb992a9029
cornicheli <gabrielcornide.pg@gmail.com>
2024-05-14 11:57:50 -0300

    fix respondive card

--- products.js
+++ products.js
@@ -3,170 +3,177 @@ const Products = [
             id: 1,
             name: 'Smartphone',
             price: '$599',
-            description: 'High-performance.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_778864-MLA70971122056_082023-O.webp'
+            description: 'Iphone 14 pro max',
+            image: 'https://s13emagst.akamaized.net/products/48592/48591223/images/res_0a49cd835e308621c4ce9f501418d2a5.jpg'
       },
       {
             id: 2,
             name: 'Laptop',
             price: '$999',
-            description: 'Powerful laptop.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_992728-MLU72761228304_112023-O.webp'
+            description: 'Asus rog',
+            image: 'https://www.asus.com/media/Odin/Websites/global/ProductLine/20200824120814.jpg'
       },
       {
             id: 3,
             name: 'Coffee Maker',
             price: '$49.99',
-            description: 'Automatic coffee.',
+            description: 'Cafetera Hudson',
             image: 'https://http2.mlstatic.com/D_NQ_NP_977975-MLU74085817099_012024-O.webp'
       },
       {
             id: 4,
             name: "Men's Watch",
             price: '$129',
-            description: 'Stylish watch.',
+            description: 'Reloj Hilfiger.',
             image: 'https://http2.mlstatic.com/D_NQ_NP_929275-MLU75592485675_042024-O.webp'
       },
       {
             id: 5,
             name: "Women's Handbag",
             price: '$79.99',
-            description: 'Trendy handbag.',
+            description: 'Cartera Gures.',
             image: 'https://http2.mlstatic.com/D_NQ_NP_871464-MLA71688023530_092023-O.webp'
       },
       {
             id: 6,
             name: 'Wireless Headphones',
             price: '$149',
-            description: 'Premium wireless.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_871884-MLU72597569891_102023-O.webp'
+            description: 'Auriculares Hyperx Cloud',
+            image: 'https://http2.mlstatic.com/D_NQ_NP_832575-MLA46273166067_062021-O.webp'
       },
       {
             id: 7,
             name: 'Fantasy Novel',
             price: '$19.99',
-            description: 'Bestselling fantasy.',
+            description: 'Libro de Fantasia',
             image: 'https://http2.mlstatic.com/D_NQ_NP_711528-MLU75151636463_032024-O.webp'
       },
       {
             id: 8,
             name: 'Cookbook',
             price: '$24.99',
-            description: 'Collection of delicious.',
+            description: 'Libro de Cocina.',
             image: 'https://http2.mlstatic.com/D_NQ_NP_839097-MLU52601956062_112022-O.webp'
       },
       {
             id: 9,
             name: 'Slow Cooker',
             price: '$59.99',
-            description: 'Programmable slow cooker.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_974804-MLU75357562149_032024-O.webp'
+            description: 'Olla Hudson',
+            image: 'https://http2.mlstatic.com/D_NQ_NP_884012-MLA71679416405_092023-O.webp'
       },
       {
             id: 10,
             name: 'Sunglasses',
             price: '$34.99',
-            description: 'Stylish sunglasses with UV protection.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_648592-MLU75305244806_032024-O.webp'
+            description: 'Notebook Acer Nitro',
+            image: 'https://compucordoba.com.ar/img/Public/1078/producto-1078-producto-acer-nitro-5-an515-55-55m1-21-79131-7163.jpg'
       },
       {
             id: 11,
             name: 'Smart TV',
             price: '$799',
-            description: 'High-definition smart TV with streaming capabilities.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_748098-MLU75806266862_042024-O.webp'
+            description: 'Smart Samsung TV',
+            image: 'https://jumboargentina.vtexassets.com/arquivos/ids/812645/Led-43-Samsung-Un43au7000gczb-Uhd-4k-1-879206.jpg?v=638440521321370000'
       },
       {
             id: 12,
             name: 'Air Fryer',
             price: '$89.99',
-            description: 'Compact air fryer for healthy frying with little to no oil.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_807565-MLU72748192047_112023-O.webp'
+            description: 'Freidora de Aire Gadnic',
+            image: 'https://www.bidcom.com.ar/publicacionesML/productos/FREI0003/1000x1000-FREI0003.jpg'
       },
       {
             id: 13,
             name: 'Self-Help Book',
             price: '$14.99',
-            description: 'Practical guide to personal development and growth.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_950098-MLA72147793180_102023-O.webp'
+            description: 'Somier Piero',
+            image: 'https://images.fravega.com/f500/75e6dc022b63530ab7e4c24fd109007b.jpg'
       },
       {
             id: 14,
             name: 'Leather Jacket',
             price: '$199',
-            description: 'Classic leather jacket for timeless style.',
+            description: 'Chaqueta de Cuero',
             image: 'https://http2.mlstatic.com/D_NQ_NP_950927-MLA76265626359_052024-O.webp'
       },
       {
             id: 15,
             name: 'Wireless Speaker',
             price: '$79.99',
-            description: 'Portable wireless speaker for on-the-go music.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_922205-MLA41374470987_042020-O.webp'
+            description: 'Parlante JBL Bluetooth.',
+            image: 'https://www.digitarinformatica.com.ar/wp-content/uploads/2022/04/Parlante-bluetooth-JBL-Flip-5-gris-20w-2.jpg'
       },
       {
             id: 16,
             name: 'Coffee Grinder',
             price: '$29.99',
-            description: 'Electric coffee grinder for freshly ground coffee.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_600112-MLA53702411340_022023-O.webp'
+            description: 'Cafetera SmartLife',
+            image: 'https://smartlifear.vtexassets.com/arquivos/ids/155459-800-auto?v=638127683456300000&width=800&height=auto&aspect=true'
       },
       {
             id: 17,
             name: 'Mystery Novel',
             price: '$16.99',
-            description: 'Page-turning mystery novel with unexpected twists.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_864912-MLU69496908453_052023-O.webp'
+            description: 'Colchon Piero.',
+            image: 'https://dcdn.mitiendanube.com/stores/001/125/330/products/21-8f2a1e251d32757f0116237073088925-640-0.jpg'
       },
       {
             id: 18,
             name: 'Running Shoes',
             price: '$79.99',
-            description: 'Comfortable running shoes with cushioned support.',
-            image: 'https://http2.mlstatic.com/D_NQ_NP_610461-MLA75960303037_042024-O.webp'
+            description: 'Zapatilla Adiddas',
+            image: 'https://www.dexter.com.ar/on/demandware.static/-/Sites-365-dabra-catalog/default/dw10af208e/products/ADGW6511/ADGW6511-6.JPG'
       },
       {
             id: 19,
             name: 'Tablet',
             price: '$299',
-            description: 'Versatile tablet for work, entertainment, and more.',
+            description: 'Tablet Samsung',
             image: 'https://http2.mlstatic.com/D_NQ_NP_892038-MLU74328290469_012024-O.webp'
       },
       {
             id: 20,
             name: 'Rice Cooker',
             price: '$49.99',
-            description: 'Automatic rice cooker for perfectly cooked rice every time.',
+            description: 'Hervidora Automatica',
             image: 'https://http2.mlstatic.com/D_NQ_NP_837539-MLA76155257154_052024-O.webp'
       },
       {
             id: 21,
             name: 'Monitor Gamer Zowie',
             price: '$18.99',
-            description: 'Exciting science fiction novel set in a distant future.',
+            description: 'Monitor Zowie',
             image: 'https://http2.mlstatic.com/D_NQ_NP_688223-MLU72580225214_112023-O.webp'
       },
       {
             id: 22,
             name: 'Backpack Arena',
             price: '$49.99',
-            description: 'Durable backpack for carrying essentials on the go.',
+            description: 'Mochila Arena',
             image: 'https://http2.mlstatic.com/D_NQ_NP_929967-MLU74157779910_012024-O.webp'
       },
       {
             id: 23,
             name: 'Smart Watch',
             price: '$149',
-            description: 'Multi-functional smart watch with fitness tracking.',
+            description: 'Reloj Digital',
             image: 'https://http2.mlstatic.com/D_NQ_NP_627143-MLU70027968091_062023-O.webp'
       },
       {
             id: 24,
             name: 'Toaster',
             price: '$29.99',
-            description: 'Stylish toaster for perfectly toasted bread every time.',
+            description: 'Tostadora Peaboy',
             image: 'https://http2.mlstatic.com/D_NQ_NP_960753-MLU75808848636_042024-O.webp'
-      }
+      },
+      {
+            id: 25,
+            name: 'Smartphone',
+            price: '$149',
+            description: 'Celular Samsung s24 Ultra',
+            image: 'https://images.tokopedia.net/img/cache/500-square/VqbcmM/2024/2/9/2ec9bc74-bb90-407c-b530-fc1f40895939.jpg'
+      },
 ];
 
 const DiscountOne = [
